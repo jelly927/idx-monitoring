214ce629c39dd4dfabda3bd2a6993fcc0c21642a
idxlive <hjaelim0@gmail.com>
2026-09-11 08:38:03 +0700

    remove 68 code/config/cache files (moved to private) [skip ci]

--- share.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 >nul
-cd /d "%~dp0"
-title IDX Live - make share zip
-set PY=py
-where py >nul 2>&1
-if errorlevel 1 set PY=python
-%PY% make_share.py
-pause
--- upload.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 >nul
-cd /d "%~dp0"
-title IDX Live - prepare GitHub upload
-set PY=py
-where py >nul 2>&1
-if errorlevel 1 set PY=python
-%PY% prepare_upload.py
-pause
